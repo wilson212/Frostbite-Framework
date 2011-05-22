b8368b7740b48c0ff66fe0657ca404aa350bb026
Steven Wilson <wilson.steven10@yahoo.com>
2011-05-21 20:02:52 -0500

    [27] Added input class and finished session reading

---
+++ core/library/Input.php
@@ -0,0 +1,109 @@
+<?php
+class Input
+{
+
+	var $user_agent = FALSE;
+	var $ip_address = FALSE;
+
+	public function __construct()()
+	{
+		// This is truely not needed!
+		$_GET = array();
+	}
+   
+
+/*
+| ---------------------------------------------------------------
+| Method: post()
+| ---------------------------------------------------------------
+|
+| Returns a $_POST variable
+|
+| @Param: $var - variable name to be returned
+|
+*/
+	public function post($var)
+	{
+		if (!isset($_POST[$var]))
+		{
+			return FALSE;
+		}
+		return trim($_POST[$var]);
+	}
+	
+/*
+| ---------------------------------------------------------------
+| Method: cookie()
+| ---------------------------------------------------------------
+|
+| Returns a $_COOKIE variable
+|
+| @Param: $name - variable name to be returned
+|
+*/
+	public function cookie($name)
+	{
+		if (!isset($_COOKIE[$name]))
+		{
+			return FALSE;
+		}
+		return trim($_COOKIE[$name]);
+	}
+	
+/*
+| ---------------------------------------------------------------
+| Method: user_agent()
+| ---------------------------------------------------------------
+|
+| Returns the browser name the user is using
+|
+*/
+	public function user_agent()
+    {
+        if ($this->user_agent !== FALSE)
+        {
+            return $this->user_agent;
+        }
+
+        $this->user_agent = ( ! isset($_SERVER['HTTP_USER_AGENT'])) ? FALSE : $_SERVER['HTTP_USER_AGENT'];
+
+        return $this->user_agent;
+    }
+
+/*
+| ---------------------------------------------------------------
+| Method: ip_address()
+| ---------------------------------------------------------------
+|
+| Returns the users IP address
+|
+*/	
+	public function ip_address()
+    {
+		// Return it if we already determined the IP
+        if ($this->ip_address !== FALSE)
+        {
+            return $this->ip_address;
+        }
+        
+		// Check to see if the server has the IP address
+        if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] != '')
+        {
+            $this->ip_address = $_SERVER['REMOTE_ADDR'];
+        }
+        elseif(isset($_SERVER['HTTP_CLIENT_IP']) && $_SERVER['HTTP_CLIENT_IP'] != '')
+        {
+            $this->ip_address = $_SERVER['HTTP_CLIENT_IP'];
+        }
+
+		// If we still have a FALSE IP address, then set to 0's
+        if ($this->ip_address === FALSE)
+        {
+            $this->ip_address = '0.0.0.0';
+        }
+
+        return $this->ip_address;
+	}
+
+}
+// EOF 
--- core/library/Session.php
+++ core/library/Session.php
@@ -9,6 +9,12 @@ class Session
 		// start the session
 		$this->start_session();
 		
+		// Init the loader class
+		$this->load = load_class('Loader');
+		
+		// load the Input class
+		$this->load->library('Input');
+		
 		// Check for session data. If there is none, create it.
 		if(!$this->read())
 		{
@@ -16,6 +22,14 @@ class Session
 		}
 	}
 	
+/*
+| ---------------------------------------------------------------
+| Method: start_session()
+| ---------------------------------------------------------------
+|
+| Only starts a session if its not already set
+|
+*/
 	function start_session()
 	{
 		if(!$this::$started)
@@ -24,24 +38,44 @@ class Session
 			$this::$started = true;
 		}
 	}
-	
+
+/*
+| ---------------------------------------------------------------
+| Method: create()
+| ---------------------------------------------------------------
+|
+| Creates session data.
+|
+*/	
 	function create()
 	{
 		$_SESSION['data'] = array();
 	}
-	
+
+/*
+| ---------------------------------------------------------------
+| Method: read()
+| ---------------------------------------------------------------
+|
+| Read session data, and cookies to determine if our session
+| is still alive
+|
+*/	
 	function read()
 	{
+		// Here we check for session data
 		if(isset($_SESSION['data']))
 		{
 			return TRUE;
 		}
-		elseif(isset($_COOKIE['data']))
+		
+		// If no session data, Check for a cookie
+		elseif($this->input->cookie('data') != FALSE)
 		{
 			// Read cookie
-			list($cookie['user_id'], $cookie['session_token']) = @unserialize(stripslashes($_COOKIE['data']));
+			list($cookie['id'], $cookie['session_token']) = @unserialize( stripslashes($this->input->cookie('data')) );
 			
-			// Do some checks
+			// Are we storing session data in the database?
 			if(config('session_use_database', 'Core'))
 			{
 				$id = config('session_database_id', 'Core');
@@ -54,10 +88,38 @@ class Session
 				// Get the database result
 				$DB->select('*')->from($table_name)->where('token', $cookie['session_token'])->query();
 				$Get = $DB->result();
+				
+				// Check user agent
+				if($Get['user_agent'] != $this->input->user_agent)
+				{
+					return FALSE;
+				}
+				
+				// check users IP address
+				elseif($Get['ip_address'] != $this->input->ip_address)
+				{
+					return FALSE;
+				}
+				
+				// All is good, return the cookie ID
+				else
+				{
+					return $cookie['id'];
+				}
 			}
+			
+			// No sessions in database
+			else
+			{
+				return FALSE;
+			}
+		}
+		
+		// No cookie :(
+		else
+		{
 			return FALSE;
 		}
-		return FALSE;
 	}
 	
 	function write()
@@ -74,21 +136,51 @@ class Session
 	{
 	
 	}
-	
+
+/*
+| ---------------------------------------------------------------
+| Method: get()
+| ---------------------------------------------------------------
+|
+| Returns a $_SESSION variable
+|
+| @Param: $name - variable name to be returned
+|
+*/	
 	function get($name)
 	{
         return $_SESSION['data'][$name]; 
 	}
 	
+/*
+| ---------------------------------------------------------------
+| Method: set()
+| ---------------------------------------------------------------
+|
+| Sets a $_SESSION variable
+|
+| @Param: $name - variable name to be set
+| @Param: $value - value of the variable
+|
+*/
 	function set($name, $value)
 	{		
         $_SESSION['data'][$name] = $value;       
         return true; 
 	}
-	
+
+/*
+| ---------------------------------------------------------------
+| Method: unset()
+| ---------------------------------------------------------------
+|
+| Unsets a $_SESSION variable
+|
+*/	
 	function unset($name)
 	{		
         unset($_SESSION['data'][$name]);       
         return true; 
 	}
 }
+// EOF
